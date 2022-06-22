552a3e9bca51feefd3561a20a0f9ddc887aa7ace
Mathieu_hstn <mathieu.hostyn@gmail.com>
2022-06-22 01:40:30 -0400

    Update requirements.txt and add first draft for box_chart (viz 4)

---
+++ box_graph.py
@@ -0,0 +1,23 @@
+import pandas as pd
+import preprocess
+import plotly.express as px
+
+
+# Get the data (temporary solution until data are processed)
+with open('./OLTCresults.csv', encoding='utf-8') as data_file:
+    oltc_data = pd.read_csv(data_file)
+
+data_time = oltc_data['tapOperationTime']
+
+def plot_box_chart(tap_nbr, tap_operation_time):
+    fig = px.box(tap_operation_time, 
+        x = tap_nbr, 
+        y = tap_operation_time,
+        )
+
+plot_box_chart(oltc_data['tapAfter'], oltc_data['tapOperationTime']) # temporary until data are processed. 
+                                                                     # function call will be done in app.py file
+
+
+
+
