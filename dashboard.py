54806abaf5d04a8a855e95018b958c4c77ccf902
slimanedev <slimane.agl@gmail.com>
2022-06-23 15:40:14 -0400

    dashboard

--- dashboard.py
+++ dashboard.py
@@ -6,16 +6,31 @@ from dash import dcc
 from dash.dependencies import Input, Output, State
 import plotly.express as px
 import pandas as pd
+import plotly.graph_objects as go
+import plotly.io as pio
 
 #Use the preprocess to manage the data. You can add fucntions if it's necessary
 
-df = pd.read_csv('./fakedata_to_delete.csv')
+df = pd.read_csv('./OLTCresults.csv')
+
+print(df.head(5))
+
+
+fig=px.bar(df, barmode='group', x='tapBefore',
+                y=['tapPowerLossAmp'],
+                title="Power Loss average per tap (Kw)",
+                labels={'value':'Power(Kw)','tapBefore':'Taps'},
+
+                )
+fig.update_traces(marker_color='blue')
+
 
 layout = html.Div([
             html.H1('Tap changer performance',
                     style={'textAlign':'center'}),
             dcc.Graph(id='bargraph',
-                    figure=px.bar(df, barmode='group', x='Years',
-                    y=['Attribut1', 'Attribut2']))
+                    figure=fig,
+                    )
                 ]
-    )
+)
+
