02b00ba0f444b2677ed5fa676963b08a568cff89
Alale Asaran <108030167+alale70@users.noreply.github.com>
2022-06-23 22:16:26 -0400

    Update page8.py

--- page8.py
+++ page8.py
@@ -17,18 +17,3 @@ def histogram_viz8(df):
 )
     return fig
 
-def violin_vis8(df,year,month):
-    df2=df
-    df2['Date'] = pd.to_datetime(df2['Date'])
-    df2 = df2.loc[df2['Date'].dt.year == year]
-    df2 = df2.loc[df2['Date'].dt.month == month]
-    fig = px.violin(df2, x="Date", y="tapCircCurrAmp", title="Circulating Current Amplitude") 
-    fig.update_layout(
-    yaxis_title="Circulating Current Amplitude(KA)",
-    font=dict(
-        family="Courier New, monospace",
-        size=14,
-        color="RebeccaPurple"
-    )
-)
-    return fig  
