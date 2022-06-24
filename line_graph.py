9d9d788706aa7945a4fdb022b902a59a17d3bcbb
Mathieu Hostyn <105740646+math-hstn@users.noreply.github.com>
2022-06-24 13:25:58 -0400

    Update line_graph.py

--- line_graph.py
+++ line_graph.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import preprocess
 import plotly.express as px
+import datetime as datetime
 
 def plot_line_chart(data):
 
     # data['Date'] = pd.to_datetime(data['Date'])
     # data = data.loc[(data['Date'] >= ('%d-01-01' % 2020))& (data['Date'] <= ('%d-12-31' % 2020))]
+    data['Time']= data['Time'].apply(lambda x:datetime.datetime.strptime(x, "%I:%M:%S %p")).hour
 
     fig = px.line(data,
         x = 'Time',
         y = "tapBefore",
         # color = "Date"
+        line_shape='vh'
         )
     fig.update_layout(title="Line-plot for the tap switching paterns",
         xaxis_title= "Day time",
