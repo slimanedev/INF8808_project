9888b0bd480d6976989071b0937e9bd6c59e0a17
Alale Asaran <108030167+alale70@users.noreply.github.com>
2022-06-24 12:04:21 -0400

    Update current_variation.py

--- current_variation.py
+++ current_variation.py
@@ -6,7 +6,7 @@ def plot_current_variation(df):
     fig = px.histogram(df, x="Date", y="tapCircCurrAmp") 
     fig.update_layout(
 
-        yaxis_title="Circulating Current Amplitude (kA)",
+        yaxis_title="Current Amplitude (kA)",
         yaxis = dict( tickfont = dict(size=20),title_font_size=20,range=[0, 1.6]),
         xaxis = dict( tickfont = dict(size=20),title_font_size=20)
 
