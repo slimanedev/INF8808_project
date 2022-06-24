0fa380a507c83d7f4bb9a632b389f9fc8f93074b
amir zamanidoost <84202242+Amirzamani4096@users.noreply.github.com>
2022-06-24 15:32:30 -0400

    Update area_chart_viz7.py

--- area_chart_viz7.py
+++ area_chart_viz7.py
@@ -9,14 +9,17 @@ with open('./OLTCresults.csv', encoding='utf-8') as data_file:
     oltc_data = pd.read_csv(data_file)
    
 oltc_data = preprocess.drop_irrelevant_time(oltc_data)
-oltc_data['Date'] = pd.to_datetime(oltc_data['Date'])
 
-oltc_data.sort_values(by='Date')
+def adjust_data(oltc_data):
+    oltc_data['Date'] = pd.to_datetime(oltc_data['Date'])
 
-df=oltc_data.groupby(pd.PeriodIndex(oltc_data['Date'], freq='M')).agg({'tapPowerLossAmp': (np.mean),
-                                                                       'Date':'first','tapEnergyLoss': (np.mean)})
-df['year'] = pd.DatetimeIndex(df['Date']).year
-df.head()
+    oltc_data.sort_values(by='Date')
+
+    df=oltc_data.groupby(pd.PeriodIndex(oltc_data['Date'], freq='M')).agg({'tapPowerLossAmp': (np.mean),
+                                                                           'Date':'first','tapEnergyLoss': (np.mean)})
+    df['year'] = pd.DatetimeIndex(df['Date']).year
+    df.head()
+    return df
 
 def area_plot_Energy_Loss(df):
     fig = px.area(df, x="Date", y="tapEnergyLoss",color = 'year')
@@ -30,6 +33,6 @@ def area_plot_Power_Loss(df):
     fig.update_xaxes(title_text='Date in Year')
     return fig
 
-fig = area_plot_Power_Loss(df)
-fig.show()
-#df
+#df = adjust_data(oltc_data)
+#fig = area_plot_Energy_Loss(df)
+#fig.show()
