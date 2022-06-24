0817c43257d31905e8a974e8ab17a7e94efe2196
Alale Asaran <108030167+alale70@users.noreply.github.com>
2022-06-23 22:55:05 -0400

    Update page8.py

--- page8.py
+++ page8.py
@@ -8,12 +8,11 @@ df = pd.read_csv('OLTCresults.csv')
 def histogram_viz8(df):
     fig = px.histogram(df, x="Date", y="tapCircCurrAmp", title="Circulating Current Amplitude") 
     fig.update_layout(
-    yaxis_title="Circulating Current Amplitude(KA)",
-    font=dict(
-        family="Courier New, monospace",
-        size=14,
-        color="RebeccaPurple"
-    )
+        yaxis_title="Circulating Current Amplitude(KA)",
+        yaxis = dict( tickfont = dict(size=20),title_font_size=20),
+        xaxis = dict( tickfont = dict(size=20),title_font_size=20)
+        )
+
 )
     return fig
 
