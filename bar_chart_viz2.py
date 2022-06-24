93ebef0eea47c2c1a4b14c7a6ccf31e21b9f4d81
Alale Asaran <108030167+alale70@users.noreply.github.com>
2022-06-24 02:03:26 -0400

    Update bar_chart_viz2.py

--- bar_chart_viz2.py
+++ bar_chart_viz2.py
@@ -11,9 +11,9 @@ def BarChart(df):
         tapBefore=df['tapBefore'].tolist()
         counter=collections.Counter(tapAfter)
         Number=counter.values()
-        df2=pd.DataFrame(counter.items() ,columns=['Numbert Of Tap', 'Number of Change per Year'])
+        df2=pd.DataFrame(counter.items() ,columns=['Numbert Of Tap', 'Number of Change'])
 
-        fig = px.bar(df2,x='Numbert Of Tap',y='Number of Change per Year') 
+        fig = px.bar(df2,x='Numbert Of Tap',y='Number of Change') 
 
         return fig
 
