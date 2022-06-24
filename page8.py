ec378e3276600daf6176cf2267147a9293c34072
Alale Asaran <108030167+alale70@users.noreply.github.com>
2022-06-23 21:22:47 -0400

    Update page8.py

--- page8.py
+++ page8.py
@@ -1,17 +1,6 @@
-import plotly.graph_objects as go
-import plotly.express as px
-import pandas as pd
-import matplotlib.pyplot as plt
-import plotly.figure_factory as ff
-
-
-
-df = pd.read_csv('OLTCresults.csv')
-
 def Histogram(df):
     fig = px.histogram(df, x="Date", y="tapCircCurrAmp", title="Circulating Current Amplitude") 
     fig.update_layout(
-    xaxis_title="Date(Year)",
     yaxis_title="Circulating Current Amplitude(KA)",
     font=dict(
         family="Courier New, monospace",
@@ -19,3 +8,20 @@ def Histogram(df):
         color="RebeccaPurple"
     )
 )
+    return fig
+
+def violin(df,year,month):
+    df2=df
+    df2['Date'] = pd.to_datetime(df2['Date'])
+    df2 = df2.loc[df2['Date'].dt.year == year]
+    df2 = df2.loc[df2['Date'].dt.month == month]
+    fig = px.violin(df2, x="Date", y="tapCircCurrAmp", title="Circulating Current Amplitude") 
+    fig.update_layout(
+    yaxis_title="Circulating Current Amplitude(KA)",
+    font=dict(
+        family="Courier New, monospace",
+        size=14,
+        color="RebeccaPurple"
+    )
+)
+    return fig  
