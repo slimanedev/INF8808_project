ded7f432ad55f83d88490e019635f2015ad45d4a
Maryam Abazari <97397265+Abazarii@users.noreply.github.com>
2022-06-25 19:05:17 -0400

    Update viz_six.py

--- src/viz_six.py
+++ src/viz_six.py
@@ -9,32 +9,36 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-
+import preprocess
 
 
 def dumbbell_plot(dataframe,input_year, input_month):
-    dataframe['Date'] = pd.to_datetime(dataframe['Date'])
-    df = dataframe.loc[dataframe['Date'].dt.year == int(input_year)]
-    dff = df.loc[df['Date'].dt.month == int(input_month)]
+    
+    #Filter the dataframe by selected year and selected month, making sure they fall in the desired range.
+    dff = preprocess.filter_by_year_month(dataframe, input_year, input_month)
 
+    #plot the dumbbell plot
     fig = go.Figure()
     fig.add_trace(go.Scatter(x=dff["Time"], y=dff["tapTime_PowLoss"], mode = 'markers', name='Tap PowLoss Time'))
     fig.add_trace(go.Scatter(x=dff["Time"], y=dff["tapOperationTime"], mode = 'markers', name="Tap Operation Time"))
+    
+    #updating the layout to set title, hover mode,...
     fig.update_layout(
-        title="Tap PowLoss Time and Tap Operation Time for year: {}, Month: {}".format(input_year, input_month),
+        title="Tap Power Loss Time and Tap Operation Time for year: {}, Month: {}".format(input_year, input_month),
         xaxis_title="Time",
         xaxis=dict(showgrid=False,showticklabels=False),
         yaxis=dict(showgrid=False),
         hovermode="x unified",
-        #plot_bgcolor = 'white',
+        legend_title='<b> Time </b>',
         font=dict( color="RebeccaPurple"))
+    
+    #adding the vertical lines to the dumbbell plot
     for i in range(dff.shape[0]):
         fig.add_shape(
             type="line",
             layer="below",
             x0=dff['Time'].iloc[i], y0=dff['tapTime_PowLoss'].iloc[i],
             x1=dff['Time'].iloc[i], y1=dff['tapOperationTime'].iloc[i])
-    fig.update_layout(legend_title='<b> Time </b>')
     return fig
 
     
