b6ea056609d5ca62b532b48e3bed7f5bf256ceae
Alale Asaran <108030167+alale70@users.noreply.github.com>
2022-06-26 17:37:10 -0400

    Update viz2_bar_chart.py

--- src/viz2_bar_chart.py
+++ src/viz2_bar_chart.py
@@ -1,6 +1,7 @@
 import plotly.express as px
 import pandas as pd
 import collections
+import template
 
 
 def BarChart(df):
@@ -10,7 +11,7 @@ def BarChart(df):
         df2=pd.DataFrame(counter.items() ,columns=['Tap position', 'Number of Change'])
 
         fig = px.bar(df2,x='Number of Change',y='Tap position', orientation='h') 
-
+        fig.update_traces(hovertemplate =template.get_hover_template_viz2())
         fig.update_layout(
         title="The number of tap changes of each tap position")
         return fig
