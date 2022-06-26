c88f23a70a9066955ad2d0177917dd8d1676638e
Mathieu Hostyn <mathieu.hostyn@gmail.com>
2022-06-26 01:13:06 -0400

    Update viz1

--- src/viz1_line_chart.py
+++ src/viz1_line_chart.py
@@ -7,17 +7,17 @@ def plot_line_chart(data):
 
     # data['Date'] = pd.to_datetime(data['Date'])
     # data = data.loc[(data['Date'] >= ('%d-01-01' % 2020))& (data['Date'] <= ('%d-12-31' % 2020))]
-    data['Time']= data['Time'].apply(lambda x:datetime.datetime.strptime(x, "%I:%M:%S %p")).hour
+    # data['Time']= data['Time'].apply(lambda x:datetime.datetime.strptime(x, "%I:%M:%S %p")).hour
 
     fig = px.line(data,
-        x = 'Time',
+        x = "Date",
         y = "tapBefore",
         # color = "Date"
         line_shape='vh'
         )
-    fig.update_layout(title="Line-plot for the tap switching paterns",
+    fig.update_layout(title="Line-plot for the tap switching patterns",
         xaxis_title= "Day time",
-        yaxis_title= 'Tap number'
+        yaxis_title= "Tap number"
     )
 
     return fig
