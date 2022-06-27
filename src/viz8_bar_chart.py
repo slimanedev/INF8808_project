9db6755e3b0fd79f1648e37f6f6a87fdc2c7d386
Alale Asaran <108030167+alale70@users.noreply.github.com>
2022-06-26 20:34:32 -0400

    Update viz8_bar_chart.py

--- src/viz8_bar_chart.py
+++ src/viz8_bar_chart.py
@@ -14,12 +14,12 @@ def get_monthly_current_plot(df):
         df_year = df[df.year==year]
         df_year = df_year.sort_values(by=['month'])
         fig.add_trace(go.Bar(x=df_year.month, y=df_year['tapCircCurrAmp'], 
-                            name=year))
+                            name=year, text = ["2015", "2016", "2017", "2018", "2019", "2030"],
+                            hovertemplate =template.get_hover_template_viz8()))
 
     fig.update_xaxes(categoryorder='array', categoryarray= month)
     fig.update_yaxes(title='Average tap circulating current (KA)')
     fig.update_layout(title="Average tap circulating current over months of the years")
-    fig.update_traces(hovertemplate =template.get_hover_template_viz8())
 
 
     return fig
