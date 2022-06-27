dcc35243cb17297f416fd8e54809692a008dfeee
slimanedev <slimane.agl@gmail.com>
2022-06-26 23:45:48 -0400

    Viz7 : deleted data processing function

--- src/viz7_area_chart.py
+++ src/viz7_area_chart.py
@@ -2,24 +2,7 @@ import pandas as pd
 import preprocess
 import plotly.express as px
 import datetime as dt
-import numpy as np
 
-# Get the data (temporary solution until data are processed)
-with open('./OLTCresults.csv', encoding='utf-8') as data_file:
-    oltc_data = pd.read_csv(data_file)
-   
-oltc_data = preprocess.drop_irrelevant_time(oltc_data)
-
-def adjust_data(oltc_data):
-    oltc_data['Date'] = pd.to_datetime(oltc_data['Date'])
-
-    oltc_data.sort_values(by='Date')
-
-    df=oltc_data.groupby(pd.PeriodIndex(oltc_data['Date'], freq='M')).agg({'tapPowerLossAmp': (np.mean),
-                                                                           'Date':'first','tapEnergyLoss': (np.mean)})
-    df['year'] = pd.DatetimeIndex(df['Date']).year
-    df.head()
-    return df
 
 def area_plot_Energy_Loss(df):
     fig = px.area(df, x="Date", y="tapEnergyLoss",color = 'year')
@@ -33,6 +16,3 @@ def area_plot_Power_Loss(df):
     fig.update_xaxes(title_text='Date in Year')
     return fig
 
-#df = adjust_data(oltc_data)
-#fig = area_plot_Energy_Loss(df)
-#fig.show()
