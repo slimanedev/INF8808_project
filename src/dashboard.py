2021cff30ef9840fe8cc3c2e6f79161f4ab8e87b
slimanedev <slimane.agl@gmail.com>
2022-06-26 20:59:36 -0400

    dashboard update2

--- src/dashboard.py
+++ src/dashboard.py
@@ -12,11 +12,6 @@ import plotly.io as pio
 PATH = pathlib.Path(__file__).parent
 DATA_PATH = PATH.joinpath("data").resolve()
 
-colors = {
-    'background': '#000000',
-    'text': '#111111'
-}
-
 # Get the data
 oltc_data = pd.read_csv(DATA_PATH.joinpath("OLTCresults.csv"))
 total_power_loss=round(sum(oltc_data['tapPowerLossAmp']),2)
@@ -26,8 +21,10 @@ total_tap_change_time=round(sum(oltc_data['tapOperationTime']),2)
 
 #Get the main figure
 fig=go.Figure(go.Bar(x=oltc_data['tapBefore'], y=oltc_data['tapPowerLossAmp']))
-fig.update_traces(marker_color='rgb(158,202,225)', marker_line_color='rgb(8,48,107)',
+fig.update_traces(marker_color='blue', marker_line_color='blue',
                   marker_line_width=1.5, opacity=0.6)
+fig.update_layout(title= 'Power Loss Average per Tap (kw)',
+                    xaxis_title='Taps',)
 
 #The layout of the dashboard
 layout = html.Div(className='content', children=[
@@ -41,21 +38,21 @@ layout = html.Div(className='content', children=[
         html.Div(className='content', children=[
                 html.Div(children=[
                         html.H4('Total Power Loss (kw)',style={'textAlign':'center'}),
-                        html.H4(total_power_loss,style={'textAlign':'center','padding': 10,'textColor':'blue'}),
+                        html.H4(total_power_loss,style={'textAlign':'center','padding': 10,'color': 'blue'}),
                         html.Br(),
                         
                         html.H4('Total Circulating Current Amplitude (kA)',style={'textAlign':'center'}),
-                        html.H4(total_circulating_current,style={'textAlign':'center','padding': 10}),
+                        html.H4(total_circulating_current,style={'textAlign':'center','padding': 10,'color': 'blue'}),
                         html.Br(),
                 ], style={'padding': 10, 'flex': 1}),
 
                 html.Div(children=[
                         html.H4('Total Energy Loss Quantity (kj)',style={'textAlign':'center'}),
-                        html.H4(total_energy_loss,style={'textAlign':'center','padding': 10}),
+                        html.H4(total_energy_loss,style={'textAlign':'center','padding': 10,'color': 'blue'}),
                         html.Br(),
 
                         html.H4('Total Tap Change Operation Time (s)',style={'textAlign':'center'}),
-                        html.H4(total_tap_change_time,style={'textAlign':'center','padding': 10}),
+                        html.H4(total_tap_change_time,style={'textAlign':'center','padding': 10,'color': 'blue'}),
                         html.Br(),
                         ], style={'padding': 10, 'flex': 1})
         ],style={'display': 'flex', 'flex-direction': 'row'})
