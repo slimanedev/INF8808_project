ae34b30a0e5cb6b8dfc707824e3ed13c7b1ef36f
Maryam Abazari <97397265+Abazarii@users.noreply.github.com>
2022-06-27 00:13:13 -0400

    Update viz6_dumbbell_chart.py

--- src/viz6_dumbbell_chart.py
+++ src/viz6_dumbbell_chart.py
@@ -9,36 +9,48 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
-import preprocess
+import preprocess, hover_template
 
 
 def dumbbell_plot(dataframe,input_year, input_month):
     
     #Filter the dataframe by selected year and selected month, making sure they fall in the desired range.
     dff = preprocess.filter_by_year_month(dataframe, input_year, input_month)
-
-    #plot the dumbbell plot
+    
+    # The dumbbell plot
     fig = go.Figure()
-    fig.add_trace(go.Scatter(x=dff["Time"], y=dff["tapTime_PowLoss"], mode = 'markers', name='Tap Power Loss Time'))
-    fig.add_trace(go.Scatter(x=dff["Time"], y=dff["tapOperationTime"], mode = 'markers', name="Tap Operation Time"))
+    fig.add_trace(go.Scatter(x=[dff["Date"].dt.day,dff["Time"]], y=dff["tapTime_PowLoss"], 
+                             mode = 'markers',marker = dict(color='#483D8B'),
+                             name='Tap Power Loss Time'))
+    fig.add_trace(go.Scatter(x=[dff["Date"].dt.day,dff["Time"]], y=dff["tapOperationTime"], 
+                             mode = 'markers', marker = dict(color='#FF6EB4'),
+                             name="Tap Operation Time"))
     
-    #updating the layout to set title, hover mode,...
-    fig.update_layout(
-        title="Tap Power Loss Time and Tap Operation Time for year: {}, Month: {}".format(input_year, input_month),
-        xaxis_title="Time",
-        xaxis=dict(showgrid=False,showticklabels=False),
-        yaxis=dict(showgrid=False),
-        hovermode="x unified",
-        legend_title='<b> Time </b>',
-        font=dict( color="RebeccaPurple"))
-    
-    #adding the vertical lines to the dumbbell plot
+    # Add vertical lines for dumbbell plot
     for i in range(dff.shape[0]):
         fig.add_shape(
             type="line",
+            line = dict(color='#282828'),
             layer="below",
-            x0=dff['Time'].iloc[i], y0=dff['tapTime_PowLoss'].iloc[i],
-            x1=dff['Time'].iloc[i], y1=dff['tapOperationTime'].iloc[i])
+            x0=i, y0=dff['tapTime_PowLoss'].iloc[i],
+            x1=i, y1=dff['tapOperationTime'].iloc[i])
+    
+    # Set hover template for bumbbell plot
+    #fig.update_traces(customdata=dff[['Date', 'tapOperationTime', "tapTime_PowLoss"]],
+    #                  hovertemplate=hover_template.get_dumbbell_hover_template())
+    
+    # Updating the layout
+    fig.update_layout(
+        title="Tap Power Loss Time and Tap Operation Time for year: {}, Month: {}".format(input_year, input_month),
+        xaxis_title="Time",
+        xaxis=dict(title = 'Day-And-Time',gridcolor='#BDBDBD' ,side = 'bottom', position = 0.01, type='multicategory',
+                   tickfont=dict(size=10),ticklabeloverflow = 'hide past domain'),
+        yaxis=dict(showgrid=False),
+        plot_bgcolor = 'beige',
+        legend_title='<b> Time </b>',
+        width=1500,
+        font=dict(color="RebeccaPurple"))
+
     return fig
 
     
