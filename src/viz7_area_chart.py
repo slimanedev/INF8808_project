84913e6e803c6ec631540023818b274d813a014d
amir zamanidoost <84202242+Amirzamani4096@users.noreply.github.com>
2022-06-28 15:31:44 -0400

    Update viz7_area_chart.py

--- src/viz7_area_chart.py
+++ src/viz7_area_chart.py
@@ -10,7 +10,7 @@ def area_plot_Energy_Loss(data):
     )
 
     fig.update_xaxes(title_text = 'Date in Year')
-    fig.update_yaxes(title_text = 'Energy Loss')
+    fig.update_yaxes(range = [0, .026], title_text = 'Energy Loss')
     return fig
 
 def area_plot_Power_Loss(data):
@@ -19,6 +19,6 @@ def area_plot_Power_Loss(data):
         y = "tapPowerLossAmp",
     )
     fig.update_xaxes(title_text = 'Date in Year')
-    fig.update_yaxes(title_text = 'Power Loss Average Amplitude (kw)')
+    fig.update_yaxes(range = [0, 1], title_text = 'Power Loss Average Amplitude (kw)')
     return fig
     
